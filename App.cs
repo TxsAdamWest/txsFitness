901ef770994a89c088e157295e0194024968d2e9
txsadamwest <twinadanz@gmail.com>
2017-02-27 16:59:00 -0600

    created test exe

--- App.cs
+++ App.cs
@@ -3,6 +3,7 @@ class App
 	static void Main(){
 		//Main method.  
 		//Prompt the user for minutes exercised
+		System.Console.Write("Enter how many minutes you exercised: ");
 		//Add minutes exercised to the total
 		//Display total minutes exercised to the screen
 		//Repeat
