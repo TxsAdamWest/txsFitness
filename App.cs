2898ddbafaf263ad57b99424da5c529e74bc81ab
txsadamwest <twinadanz@gmail.com>
2017-02-27 16:43:32 -0600

    steps added

--- App.cs
+++ App.cs
@@ -1,6 +1,10 @@
 class App 
 {
 	static void Main(){
-		//Main method.
+		//Main method.  
+		//Prompt the user for minutes exercised
+		//Add minutes exercised to the total
+		//Display total minutes exercised to the screen
+		//Repeat
 	}
 }
