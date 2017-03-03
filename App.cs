9d222739dd9ed48792c7e17e8388eb34c6cf1f15
txsadamwest <twinadanz@gmail.com>
2017-03-02 23:51:47 -0600

    initalized entry variable

--- App.cs
+++ App.cs
@@ -4,6 +4,11 @@ class App
 		//Main method.  
 		//Prompt the user for minutes exercised
 		System.Console.Write("Enter how many minutes you exercised: ");
+
+		string entry = System.Console.ReadLine(); // Reads what the user types and returns it to the program until the return key is pressed.
+		 // Initialized the variable entry equal to the value of the return value of the System.Console.Readline() method.
+
+
 		//Add minutes exercised to the total
 		//Display total minutes exercised to the screen
 		//Repeat
